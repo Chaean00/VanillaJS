7ca4c559b633baacaa5eceebb5dc58adef82b28f
chaean <jtkim9965@gmail.com>
2022-12-27 18:31:46 +0900

    Login ver2

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function OnLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+
+function OnLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
-loginButton.addEventListener("click", OnLoginBtnClick);
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", OnLoginSubmit);
+} else {
+    // show the greeting
+    paintGreeting(savedUsername);
+}
+
